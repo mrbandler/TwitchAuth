60331178d93ef1b63ec5d81c5398983e3483b41f
mrbandler <baudler.michael@gmail.com>
2017-08-19 20:47:32 +0200

    Plugin created.

---
+++ Source/TwitchAuth/Private/TwitchAuthBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "TwitchAuthBPLibrary.h"
+#include "TwitchAuth.h"
+
+UTwitchAuthBPLibrary::UTwitchAuthBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UTwitchAuthBPLibrary::TwitchAuthSampleFunction(float Param)
+{
+	return -1;
+}
+
