908dd6c659efd029c8036f6362af1467b9f2100e
mrbandler <mrbandler@fivefingergames.com>
2020-02-28 18:43:30 +0100

    release(v1.1.7): Bumped version number

--- Package.bat
+++
@@ -1,9 +0,0 @@
-rem Build the plugin
-call RunUAT.bat BuildPlugin -Plugin="%CD%\TwitchAuth.uplugin" -Package="%CD%\Build\TwitchAuth" -Rocket
-
-rem Zip the plugin
-rd /s /q "./Build/TwitchAuth/Intermediate"
-rd /s /q "./Build/TwitchAuth/Binaries"
-7z a ./Build/TwitchAuth.zip ./Build/TwitchAuth/*
-
-pause
